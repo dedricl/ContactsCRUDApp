34b15c1b5b4598f42784b591e4ab81d8e880a791
Dedric <dedricleftwich@gmail.com>
2022-02-16 19:21:59 -0500

    Add project files.

---
+++ ContactsCRUDApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContactsCRUDApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
