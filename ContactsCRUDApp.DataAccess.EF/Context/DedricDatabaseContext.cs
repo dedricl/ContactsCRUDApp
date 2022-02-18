051180918c5498ecad64e05db6d4dbbc1388c1cc
Dedric <dedricleftwich@gmail.com>
2022-02-17 20:13:21 -0500

    Add Enitity Framworks class library

---
+++ ContactsCRUDApp.DataAccess.EF/Context/DedricDatabaseContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using ContactsCRUDApp.DataAccess.EF.Models;
+
+#nullable disable
+
+namespace ContactsCRUDApp.DataAccess.EF.Context
+{
+    public partial class DedricDatabaseContext : DbContext
+    {
+        public DedricDatabaseContext()
+        {
+        }
+
+        public DedricDatabaseContext(DbContextOptions<DedricDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ContactInfo> ContactInfos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=DedricDatabase;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ContactInfo>(entity =>
+            {
+                entity.HasKey(e => e.ContactId);
+
+                entity.ToTable("ContactInfo");
+
+                entity.Property(e => e.EmailAddress)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PhoneNumber)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
